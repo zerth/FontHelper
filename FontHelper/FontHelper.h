1f82b4953e8d04ed20258e7af4b28c68d34ad55d
Mike Watters <mike@mwatters.net>
2014-05-28 14:09:41 -0600

    Initial Commit

---
+++ FontHelper/FontHelper.h
@@ -0,0 +1,13 @@
+//
+//  FontHelper.h
+//  FontHelper
+//
+//  Created by dev on 5/28/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FontHelper : NSObject
+
+@end
---
+++ FontHelper/FontHelper.m
@@ -0,0 +1,13 @@
+//
+//  FontHelper.m
+//  FontHelper
+//
+//  Created by dev on 5/28/14.
+//
+//
+
+#import "FontHelper.h"
+
+@implementation FontHelper
+
+@end
